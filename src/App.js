7df63e91581c3335224c1f3b2217f9ebc85c57f7
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-27 11:45:00 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      Hello
+    </div>
+  );
+}
+
+export default App;
