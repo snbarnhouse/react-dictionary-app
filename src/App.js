1ee75c0ae2b4d96791c1ed7d701a8c1694440d1b
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-27 17:13:48 -0500

    Adding dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/snbarnhouse/react-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source
+            </a>{" "}
+            code by{" "}
+            <a
+              href="https://dreamy-visvesvaraya-894128.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Stephanie Barnhouse
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
