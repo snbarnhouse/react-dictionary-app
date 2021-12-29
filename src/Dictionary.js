6e5a32b3f80ce98750cfdfe335fc25dfe7c36749
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-28 20:19:23 -0500

    Adding meanings and results.js files

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary () {
-    let [keyword, setKeyword] = useState(null);
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0])
+        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search (event) {
         event.preventDefault();
 
-
+//documentation: https://dictionaryapi.dev/ 
         let apiUrl =
           `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
           axios.get(apiUrl).then(handleResponse);
@@ -26,6 +29,7 @@ export default function Dictionary () {
            <form onSubmit={search}>
                <input type="search" onChange={handleKeywordChange} />
            </form>
+           <Results results={results}/>
         </div>
     )
 }
---
+++ src/Meaning.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//Looping through each meaning of the results.
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+             <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
