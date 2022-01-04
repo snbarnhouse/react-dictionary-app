9d42cb54c659bb1ef0543a34cf1a6e4e72fb55a1
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2022-01-03 19:46:23 -0500

    Adding phonetics component

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen 🔉 
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
+}
