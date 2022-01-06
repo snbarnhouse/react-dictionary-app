1b7f015c6c4ffb616e53119d4e301ea60cfb0bf5
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2022-01-05 20:16:06 -0500

    Adding alt to photos

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,7 @@ export default function Photos(props) {
                       target="_blank"
                       rel="noreferrer"
                     >
-                      <img src={photo.src.landscape} className="img-fluid" />
+                      <img src={photo.src.landscape} alt={photo.alt} className="img-fluid" />
                     </a>
                   </div>
                 );
